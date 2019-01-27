b069d5e9b31f11ee7a57fb0e1293e6c5d4f16ffa
Max Dauber <max@dauberfamily.com>
2019-01-27 14:28:41 -0600

    first file dump
    TODO: fill in methods and fix structure

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//Main.java
+/*
+ * EE422C Project 1 submission by
+ * Meyer Dauber
+ * mjd3375
+ * 16190
+ * Fall 2018
+ * Slip days used: N/A
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
